089ded656a96d4731289fc5c943ff4777d3be4ee
Brandi <brandihollee@gmail.com>
2022-11-13 11:49:28 -0500

    added routes, deleted unnecessary files

--- routes/main.js
+++ routes/main.js
@@ -1,18 +1,26 @@
 const express = require("express");
 const router = express.Router();
-const authController = require("../controllers/auth");
 const homeController = require("../controllers/home");
-const postsController = require("../controllers/posts");
-const { ensureAuth, ensureGuest } = require("../middleware/auth");
+const dictionaryController = require("../controllers/dictionary");
+const storyController = require ("../controllers/story");
 
 //Main Routes - simplified for now
 router.get("/", homeController.getIndex);
-router.get("/profile", ensureAuth, postsController.getProfile);
-router.get("/feed", ensureAuth, postsController.getFeed);
-router.get("/login", authController.getLogin);
-router.post("/login", authController.postLogin);
-router.get("/logout", authController.logout);
-router.get("/signup", authController.getSignup);
-router.post("/signup", authController.postSignup);
+router.get("/dictionary", dictionaryController.getDictionary);
+router.post("/dictionary", dictionaryController.postDictionary);
+router.delete("/dictionary", dictionaryController.deleteDictionary);
+router.post("/story", storyController.postStory);
+
+
+
+
+
+// router.get("/feed", ensureAuth, postsController.getFeed);
+// router.get("/login", authController.getLogin);
+// router.post("/login", authController.postLogin);
+// router.get("/logout", authController.logout);
+// router.get("/signup", authController.getSignup);
+// router.post("/signup", authController.postSignup);
 
 module.exports = router;
+
