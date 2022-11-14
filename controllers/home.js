0783d4827a7b235bb96d9960dbb18e1bbee82635
Brandi <brandihollee@gmail.com>
2022-11-13 19:31:20 -0500

    logic and route for adding words to dictionary.txt with focus on avoiding duplicate words

--- controllers/home.js
+++ controllers/home.js
@@ -4,7 +4,30 @@ module.exports = {
   getIndex: (req, res) => {
     res.render("index.ejs");
   },
+
   getDictionary: (req, res) => {
     res.render("dictionary.ejs", { list: list });
   },
+
+  postDictionary: (req, res) =>{
+    list.push(req.body.word);
+    let i = 0;
+    while (i < list.length-1){
+      if (list[i] == req.body.word){
+        list.pop();
+        console.error("Duplicate entry")
+        break;
+      } else{
+        i++
+      }}
+    list.sort()
+    let txtList = list.join("\n");
+    fs.writeFile('./dictionary.txt', txtList, err => {
+      if (err) {
+        console.error(err);
+      }
+    });
+    res.redirect("/dictionary");
+  },
 };
+
--- routes/main.js
+++ routes/main.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const homeController = require("../controllers/home");
-const dictionaryController = require("../controllers/dictionary.js");
 // const storyController = require ("../controllers/story");
 
 //Main Routes - simplified for now
 router.get("/", homeController.getIndex);
 router.get("/dictionary", homeController.getDictionary);
-// router.post("/dictionary", homeController.postDictionary);
+router.post("/dictionary", homeController.postDictionary);
 // router.delete("/dictionary", homeController.deleteDictionary);
 // router.post("/story", storyController.postStory);
 
